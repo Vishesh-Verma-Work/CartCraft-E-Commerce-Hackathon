7f69bb1fccb203b8129268c0493ff286ab006169
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-04-27 21:30:11 +0530

    module update

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -355,6 +355,18 @@ export default SignupPage;
 //                 <Mail className="h-5 w-5 text-gray-400" />
 //               </div>
 //               <input
+//                 id="email""h-5 w-5 text-gray-400" />
+//                 id="email"
+//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+//                 <Mail className="h-5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email""h-5 w-5 text-gray-400" />
+//                 id="email"
+//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+//                 <Mail className="h-5 w-5 text-gray-400" />
+//               </div>
+//               <input
 //                 id="email"
 //                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
 //                 <Mail className="h-5 w-5 text-gray-400" />
