64d308de96c2bb9f16c9b7becf8354fa5512635e
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-05-02 22:42:16 +0530

    Adding shadCn for ui inhancement

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -357,3 +357,43 @@ export default SignupPage;
 //               <input
 //                 id="email"
 //                 name="email"
+//                 name="email"5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+//                 <Mail className="h-5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"
+//                 name="email"5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+//                 <Mail className="h-5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"
+//                 name="email"5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+//                 <Mail className="h-5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"
+//                 name="email"5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+//                 <Mail className="h-5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"
