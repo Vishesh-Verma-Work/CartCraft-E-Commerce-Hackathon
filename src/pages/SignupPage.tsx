855135a7bac307232a14a09782463865ca899be2
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-05-03 22:17:15 +0530

    Josh tech interview prep

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -396,4 +396,40 @@ export default SignupPage;
 //               </div>
 //               <input
 //                 id="email"
+//                 name="email"5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+//                 <Mail className="h-5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+//                 <Mail className="h-5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+//                 <Mail className="h-5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
+//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+//                 <Mail className="h-5 w-5 text-gray-400" />
+//               </div>
+//               <input
+//                 id="email"
 //                 name="email"
