008d45f49dc71e6d9112710d210eafc6b02af9aa
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2025-05-04 22:50:38 +0530

    Josh tech prep

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -290,7 +290,7 @@ export default SignupPage;
 //                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
 //                 <Mail className="h-5 w-5 text-gray-400" />
 //               </div>
-//               <input
+
 //                 id="email"
 //                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
 //                 <Mail className="h-5 w-5 text-gray-400" />
@@ -302,7 +302,7 @@ export default SignupPage;
 //                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
 //                 <Mail className="h-5 w-5 text-gray-400" />
 //               </div>
-//               <input
+
 //                 id="email"
 //                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
 //                 <Mail className="h-5 w-5 text-gray-400" />
@@ -314,122 +314,27 @@ export default SignupPage;
 //                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
 //                 <Mail className="h-5 w-5 text-gray-400" />
 //               </div>
-//               <input
+
 //                 id="email"
 //                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
 //                 <Mail className="h-5 w-5 text-gray-400" />
 //               </div>
 //               <input
-//                 id="email"
-//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-//                 <Mail className="h-5 w-5 text-gray-400" />
-//               </div> flex items-center pointer-events-none">
-//                 <Mail className="h-5 w-5 text-gray-400" />
-//               </div>
-//               <input
+
+
 //                 id="email"
 //                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
 //                 <Mail className="h-5 w-5 text-gray-400" />
 //               </div>
-//               <input
+
 //                 id="email"
 //                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
 //                 <Mail className="h-5 w-5 text-gray-400" />
 //               </div>
 //               <input
-//                 id="email"
-//                 name="email"5 w-5 text-gray-400" />
-//               </div>
-//               <input
+
+
 //                 id="email"
 //                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
 //                 <Mail className="h-5 w-5 text-gray-400" />
 //               </div>
-//               <input
-//                 id="email"
-//                 name="email"5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-//                 <Mail className="h-5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"
-//                 name="email"5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-//                 <Mail className="h-5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"
-//                 name="email"5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-//                 <Mail className="h-5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"
-//                 name="email"5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-//                 <Mail className="h-5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"
-//                 name="email"5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-//                 <Mail className="h-5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-//                 <Mail className="h-5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-//                 <Mail className="h-5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-//                 <Mail className="h-5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"Name="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-//                 <Mail className="h-5 w-5 text-gray-400" />
-//               </div>
-//               <input
-//                 id="email"
-//                 name="email"
